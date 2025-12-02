5da5773aa085f5ef4090957b509a04f702450ce3
Michael Lim <michaeljoelmama@gmail.com>
2025-12-02 12:11:11 +0700

    Refactor report_delete.php for cleaner code

--- public/report_delete.php
+++ public/report_delete.php
@@ -1,21 +1,16 @@
 <?php
-// 1. Panggil Mesin
 require_once '../includes/crud_barang.php';
 
-// 2. Ambil ID yang mau dihapus
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     
-    // 3. Suruh mesin menghapus
     if (hapusBarang($id)) {
-        // Kalau berhasil, balik ke daftar
         echo "<script>alert('Laporan berhasil dihapus.'); window.location='report_list.php';</script>";
     } else {
-        // Kalau gagal
         echo "<script>alert('Gagal menghapus data.'); window.location='report_list.php';</script>";
     }
 } else {
-    // Kalau orang iseng buka file ini tanpa ID, tendang balik
     header("Location: report_list.php");
 }
+
 ?>
